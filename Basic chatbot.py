65125dc80aeebd904ef4983fc842fe6f096231fe
paridhimuttyamwar <paridhi.muttyamwar@gmail.com>
2026-07-04 15:28:08 +0530

    Add files via upload

---
+++ Basic chatbot.py
@@ -0,0 +1,29 @@
+def chatbot():
+
+    print("Hello! I am your Chatbot.")
+    print("Type 'bye' to exit.\n")
+
+    while True:
+
+        user = input("You: ").lower()
+
+        if user == "hello":
+            print("Bot: Hi!")
+
+        elif user == "how are you":
+            print("Bot: I'm fine, thanks!")
+
+        elif user == "what is your name":
+            print("Bot: I am a Python Chatbot.")
+
+        elif user == "who created you":
+            print("Bot: I was created using Python.")
+
+        elif user == "bye":
+            print("Bot: Goodbye!")
+            break
+
+        else:
+            print("Bot: Sorry, I don't understand that.")
+
+chatbot()
